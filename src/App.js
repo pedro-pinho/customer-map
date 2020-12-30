75d60db563e5eed5bc0dbd2670b812dc096c86da
Pedro Pinho <pedropinho37@gmail.com>
2020-12-30 10:42:41 -0300

    Integration with Google Maps API, already showing map and with a configurated server backend; Location fixed for now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import GoogleMap from 'google-map-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const mapStyles = {
+    width: '100%',
+    height: '100%'
+}
+
+const markerStyle = {
+    height: '50px',
+    width: '50px',
+    marginTop: '-50px'
+}
+
+const imgStyle = {
+    height: '100%'
+}
+
+
+const Marker = ({ title }) => (
+    <div style={markerStyle}>
+        <img style={imgStyle} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" alt={title} />
+        <h3>{title}</h3>
     </div>
-  );
+);
+
+class App extends Component {
+    render() {
+        return (
+            <div >
+                <GoogleMap
+                    style={mapStyles}
+                    bootstrapURLKeys={{ key: 'AIzaSyBhDHMcNUVKGWe3_JSEeNo9VIZNt6Od6wQ' }}
+                    center={{ lat: -22.335927873554713, lng: -49.0955706317051 }}
+                    zoom={14}
+                >
+                    <Marker
+                        title={'Current Location'}
+                        lat={-22.335927873554713}
+                        lng={-49.0955706317051}
+                    >
+                    </Marker>
+                </GoogleMap>
+            </div>
+        );
+    }
 }
 
 export default App;
